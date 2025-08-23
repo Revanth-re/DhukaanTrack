837c09a0c11c6a92e548012f0e25c9672aa6dadd
Revanth <revanthrevi131@gmail.com>
2025-08-23 20:05:59 +0530

    dfvf

--- OurProject/vite.config.js
+++ OurProject/vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   css: {
-    minify: 'esbuild' // use esbuild instead of lightningcss
+  css: {
+    minify: 'esbuild' // Use esbuild instead of lightningcss (avoids native binary issues)
   }
-  
-})
-
-
+});
